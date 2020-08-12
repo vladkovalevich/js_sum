0e639b5a7771c29679553fbb02375cd4291a088c
vladkovalevich <64867756+vladkovalevich@users.noreply.github.com>
2020-08-12 10:46:21 +0300

    ....

--- homeworks/hw6.js
+++ homeworks/hw6.js
@@ -4,6 +4,11 @@ import users from "./users.js";
 const getUserNames = users => {
   return users.map((user) => user.name);
 };
-
+//task 2
 console.log(getUserNames(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
+const getUsersWithEyeColor = (users, color) => {
+  return users.filter(user => user.eyeColor === color);
+};
+
+console.log(getUsersWithEyeColor(users, "blue")); 
